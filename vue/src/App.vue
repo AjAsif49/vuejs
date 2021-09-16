6249136d0c6b5d4e17ffb6a380f42da62bf5abdf
AjAsif49 <54905485+AjAsif49@users.noreply.github.com>
2021-09-16 22:28:03 +0600

    nav

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Navbar from './components/Navbar.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+    title: "Wow"
   }
+  },
+  components:{
+    Navbar
+  }
+
 }
 </script>
 
